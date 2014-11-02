c0fde03af2a24c586c46c4ffacc2f769943b6551
woodenToaster <litmusik@yahoo.com>
2014-11-02 15:13:06 -0600

    Use mocha for testing and should for assertions

--- test/testGrammar.js
+++ test/testGrammar.js
@@ -1,8 +1,18 @@
-function exec(input) {
-	return require("../parser/hackSlash").parser.parse(input);
-}
+var should = require('should');
+var parser = require('../parser/hackSlash');
 
-console.log(exec('tunic.color: "blue"'));
-console.log(exec('if 5 > 2: heal'));
-console.log(exec('every 2000: attack'));
+describe('HackSlash Parser', function() {
+	function exec(input) {
+		return parser.parse(input);
+  }
+  it('recognizes valid statements in the language', function() {
+  	exec('tunic.color: "blue"').should.be.true;
+		exec('if 5 > 2: heal').should.be.true;
+		exec('every 2000: attack').should.be.true;
+  })
+});
+
+
+
+
 
